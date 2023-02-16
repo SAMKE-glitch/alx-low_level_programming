7ccb533c945f8ea9ddecc5b4bf32a8fb5cb3687d
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-16 12:41:38 -0800

    made some changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int:%lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int:%lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float:%lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof());
+printf("Size of an int:%lu byte(s)\n", (unsigned long)sizeof());
+printf("size of a long int:%lu byte(s)\n", (unsigned long)sizeof());
+printf("size of a long long int:%lu byte(s)\n", (unsigned long)sizeof());
+printf("size of a float:%lu byte(s)\n", (unsigned long)sizeof());
 return (0);
 
 }
