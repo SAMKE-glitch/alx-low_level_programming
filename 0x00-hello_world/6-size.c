73892cd1a64710a5538012e6bdea4eb9435805f3
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-16 12:35:42 -0800

    changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - A program that prints the size of various computer types
- * Return:0 (success)
+ * Return:0(success)
  */
 int main(void)
 {
@@ -18,4 +18,5 @@ printf("size of a long int:%lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of a float:%lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
+
 }
