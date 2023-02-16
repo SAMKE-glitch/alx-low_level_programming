e04754439f70bb7a50bda33e1d66600f9047a2d6
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-16 12:05:22 -0800

    changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - A program that print a line using the printf function
- * Return:0 (success)
+ *main - A program that print a line using the printf function
+ *Return:0(success)
  */
 int main(void)
 {
