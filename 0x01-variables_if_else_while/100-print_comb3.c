e6012e3863d3565145516c93cddb5935e2813672
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-17 15:09:52 -0800

    made changes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,37 +3,29 @@
 /**
  * main - Entry point
  *
- * Return: 0 Always
+ * Return: 0 Always Success
  */
 
 int main(void)
 {
-
 	int n, b, c;
 
-	for (n = 48; n <=57; n++)
+	for (n = 48; n <= 57; n++)
 	{
-	
-		for(b = n + 1; b <= 57; b++)
+		for (b = n + 1; b <= 57; b++)
 		{
-		
 			for (c = b + 1; c <= 57; c++)
 			{
-			
 				putchar(n);
 				putchar(b);
 				putchar(c);
-				if (n !=55 || b !=56 || c !=57)
+				if (n != 55 || b != 56 || c != 57)
 				{
-				
 					putchar(',');
 					putchar(' ');
 				}
-
 			}
-		
 		}
-	
 	}
 	putchar('\n');
 	return (0);
