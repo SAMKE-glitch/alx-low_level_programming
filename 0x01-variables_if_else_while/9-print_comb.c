ba6c58d225a860072dfbdaf1ad1e338ad41f55c0
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-17 14:46:20 -0800

    corrections

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,21 +8,17 @@
 
 int main(void)
 {
-
 	int n;
-	
+
 	for (n = 48; n <= 57; n++)
 	{
-	
 		putchar(n);
-		if (n !=57)
+		if (n != 57)
 		{
-		
 			putchar(',');
 			putchar(' ');
 		}
-		
 	}
-	putchar ('\');
+	putchar('\n');
 	return (0);
 }
