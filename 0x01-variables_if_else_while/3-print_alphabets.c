df8d77022393cbb917b3f937738fa34eba5a17a5
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-17 13:51:51 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Function
+ *
+ * Return: 0 Always Success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+		putchar(n);
+	for (n =65; n <= 90; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
