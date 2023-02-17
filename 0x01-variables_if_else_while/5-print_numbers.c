bef27c1fed032f701218bb882c772b855acf59eb
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-17 14:13:14 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always Success
+ */
+
+int main (void)
+{
+
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
