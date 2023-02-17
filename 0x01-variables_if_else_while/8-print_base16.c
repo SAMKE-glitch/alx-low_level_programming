3e81378f4504d50bffacd2f663ac3cb47f43b764
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-17 14:31:19 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Function
+ *
+ * Return: 0 Always Success
+ */
+
+int main(void)
+{
+
+	int n;
+
+	for (n =48; n <= 57; n++)
+		putchar(n);
+	for (n = 97; n <= 102; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
