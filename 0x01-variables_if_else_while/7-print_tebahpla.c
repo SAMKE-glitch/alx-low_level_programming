b5ec387db2298c298d765a6cfeb508513db1950c
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-17 14:25:25 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Function
+ *
+ * Return: 0 Always Success
+ */
+
+int main(void)
+{
+
+	int n;
+
+	for (n = 122; n >= 97; n--)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
