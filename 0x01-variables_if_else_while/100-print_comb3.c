ad510c8e9ec959580bb38a38741166a21cd01201
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-17 15:05:10 -0800

    Inventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+
+	int n, b, c;
+
+	for (n = 48; n <=57; n++)
+	{
+	
+		for(b = n + 1; b <= 57; b++)
+		{
+		
+			for (c = b + 1; c <= 57; c++)
+			{
+			
+				putchar(n);
+				putchar(b);
+				putchar(c);
+				if (n !=55 || b !=56 || c !=57)
+				{
+				
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
