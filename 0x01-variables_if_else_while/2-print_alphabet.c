384d289c79086e27d900e470a6d6e4dc2c326a91
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-17 13:34:34 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - Function
+ *
+ * Return: 0 Always Success
+ */
+
+int main(void)
+{
+
+	int n;
+
+	for (n = 97; n <= 122; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
