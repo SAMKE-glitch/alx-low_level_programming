d969460f87df315ed36315f9a49f146602a77c57
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-21 08:57:08 -0800

    made some changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,9 +12,9 @@ void print_alphabet_x10(void)
 	int i;
 	char j;
 
-	for (i = 1 : i <= 10 : i++)
+	for (i = 1 ; i <= 10 ; i++)
 	{
-		for (j = 'a' : j <= 'z' : j++)
+		for (j = 'a' ; j <= 'z' ; j++)
 			_putchar(j);
 		_putchar('\n');
 	}
