0439d7e0a8030c10f672e3ede166b6396b179582
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-21 07:27:58 -0800

    printing alphabet putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - check the code
- * return: Always 0.
+ * main - main function
+ * _putchar: print the alphabet putchar
+ * Return: Always 0
  */
+
+
 int main(void)
 {
 
-	printf("_putchar\n",);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
