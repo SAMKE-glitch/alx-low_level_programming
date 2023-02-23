6f453fcd48b73b84ccfab748818642f1897d1d41
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-23 10:37:18 -0800

    function that prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - from 0 to 9 without 2 and 4
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		if (a == 2 || 4)
+			;
+		else
+			_putchar('0' + a);
+	_putchar('\n');
+}
