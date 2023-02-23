86914b7d15f4adb5ac101f9ebfafe1b90c6376d8
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-23 10:10:45 -0800

    function that prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * print_numbers - print number 0 to 9
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 8; a <= 17; a++)
+		_putchar(a);
+	_putchar('\n');
+}
