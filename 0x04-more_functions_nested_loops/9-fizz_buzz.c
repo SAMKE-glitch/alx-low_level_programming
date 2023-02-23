b882ca3398f2fdf16e23f898e20fbebd69d0fe4c
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-23 12:31:06 -0800

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", a);
+		if (a != 100)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
