631c805b7bd2cf4028597747cb5965ca390050cb
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-23 10:00:08 -0800

    function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function multiplies two digits
+ *
+ * @a: integer to check
+ * @b: integer to check
+ * Return: Results
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
