7042fe7c607fd4019104df07b39cf99d0451a44b
SAMKE-glitch <sammythemwa@gmail.com>
2023-02-23 11:12:24 -0800

    prints 10 times the numbers, from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 1 to 14 ten times
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; < 15; b++)
+		{
+			_putchar(b + '0');
+		}
+		_putchar('\n');
+	}
+}
