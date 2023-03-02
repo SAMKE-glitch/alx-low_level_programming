6e37039fdcbf2488e9dde45e4e508d25facb2d0b
SAMKE-glitch <sammythemwa@gmail.com>
2023-03-02 10:54:09 -0800

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,10 +3,10 @@
 /**
  * cap_string - capitalizes all words of a string
  *
- * @z: string parameter
+ * @x: string parameter
  * Return: capitalized version of string
  */
-char *cap_string(char *z)
+char *cap_string(char *x)
 {
 	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
@@ -14,16 +14,16 @@ char *cap_string(char *z)
 
 	int a = 0, i;
 
-	while (z[a])
+	while (x[a])
 	{
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || z[a - 1] == spc[i]) && (z[a] >= 97 && z[a] <= 122))
-			z[a] = z[a] - 32;
+			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+			x[a] = x[a] - 32;
 			i++;
 		}
 		a++;
 	}
-	return (Z);
+	return (x);
 }
