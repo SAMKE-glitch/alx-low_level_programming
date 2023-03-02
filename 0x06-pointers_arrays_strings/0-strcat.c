f19dd9039660f3f183f1e6fe0f08fcc99d5cfffb
SAMKE-glitch <sammythemwa@gmail.com>
2023-03-02 02:40:51 -0800

    made some corrections

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int destlen = 0;
-	int srclen = 0;
-	int i;
+	int i, k;
 
-	for (i = 0 ; dest[i] != '\0' ; i++)
-		destlen++; |
-	for (i = 0 ; src[i] != '\0' ; i++)
-		src++ |
-	for (i = 0 ; i <= srclen ; i++)
-		| dest[destlen + i] = src[i];
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (k = 0; src[k]; k++)
+		dest[i++] = src[k];
+	dest[i++] = '\0';
+
 	return (dest);
 }
