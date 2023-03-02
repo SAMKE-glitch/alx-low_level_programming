8751730465106cdac0f9daf82cf42a2bf3bd7c6f
SAMKE-glitch <sammythemwa@gmail.com>
2023-03-02 06:58:14 -0800

    function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncpy - function that copies string
@@ -9,11 +10,6 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
-
-	for (j = 0; j < n && src != '\0'; j++)
-		dest[j] = src[j];
-	for ( ; j < n; j++)
-		dest[j] = '\0';
+	strncpy(dest, src, n);
 	return (dest);
 }
