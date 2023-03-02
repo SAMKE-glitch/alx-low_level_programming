838458279f41642c00661dff3252baaa116939b8
SAMKE-glitch <sammythemwa@gmail.com>
2023-03-02 09:04:17 -0800

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * string_toupper - function that changes uppercase to lowercase
+ * string_toupper - function that changes string to uppercase
  * @c: string to be changed
  * Return: a lowercase characer or string.
  */
 char *string_toupper(char *c)
 {
-	int i = 0;
+	int = 0;
 
-	while (c[i] != '\0'; i++)
+	while (c[i])
 	{
-		if (c[i] >= 97 && c[i] <= 122)
+		if (c[i] >= 97 && x[i] <= 122)
 			c[i] = c[i] - 32;
+		i++;
 	}
 	return (c);
-
 }
