e1b97b31b3dc8d7742f6ee03c0cd0da32c6efa23
SAMKE-glitch <sammythemwa@gmail.com>
2023-03-08 07:01:53 -0800

    function that returns the factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - function that returns a factorial of a number
+ * @n: number
+ * Return: an integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
