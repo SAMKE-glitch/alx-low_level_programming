fa04b2564ff9525039638def4fdfd41351c14f52
SAMKE-glitch <sammythemwa@gmail.com>
2023-03-10 10:10:22 -0800

    program that prints the number of arguments passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: an array fo a command
  * Return: sucess (0)
  */
-int main(int argc, char *argv[] argc __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
