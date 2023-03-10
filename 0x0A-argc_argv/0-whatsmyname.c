5c498a5fa51ab4b021d66559c6d0a379c652efa1
SAMKE-glitch <sammythemwa@gmail.com>
2023-03-10 09:25:44 -0800

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function to print its name
+ * @argc: parameter
+ * @argv: array of a command
+ * Return: 0 for success
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+}
