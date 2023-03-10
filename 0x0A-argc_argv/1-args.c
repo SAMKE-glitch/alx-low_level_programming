8bd9625819a9d28b26a0bb2e5140958f2dc214cd
SAMKE-glitch <sammythemwa@gmail.com>
2023-03-10 10:08:31 -0800

    program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the number of arguments
+ * @argc: argc parameter
+ * @argv: an array fo a command
+ * Return: sucess (0)
+ */
+int main(int argc, char *argv[] argc __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
