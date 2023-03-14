254838ca886439d857426f339834ae9de73bdcef
SAMKE-glitch <sammythemwa@gmail.com>
2023-03-14 02:06:17 -0700

    function that returns a pointer for a duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copies the string given as a parameter
+ * @str: string to be copied
+ * Return: pointer to the new string copied
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len])
+		len++
+			duplicate = malloc(sizeof(char) * (len + 1));
+	if (duplicate == NULL)
+		return (NULL);
+	while ((duplicate[i] = str[i]) != '\0')
+		i++;
+	return (duplicate);
+}
