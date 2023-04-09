2c20fffe7cf002959a2240b0876dc3a12cd21f4e
SAMKE-glitch <sammythemwa@gmail.com>
2023-04-09 13:35:11 -0700

    creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, strlen(text_content));
+	if (text_content)
+	{
+		write(fd, text_content, strlen(text_content));
+	}
 	close(fd);
 
 	return (1);
