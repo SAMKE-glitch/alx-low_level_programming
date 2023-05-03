22149ee5d9b8107f44d02208c27d32cb33b6dcf6
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-03 00:48:28 -0700

    updated readme file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -29,7 +29,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-                                printf(", ");
+               			printf(", ");
 		}
 		printf("\n");
 	}
@@ -39,7 +39,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-                                printf(", ");
+                        	 printf(", ");
 		}
 		printf("\n");
 	}
