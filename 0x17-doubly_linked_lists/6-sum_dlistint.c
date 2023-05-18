95fcd097f29df67cd209b282cc9d879ee710d9c7
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-18 10:55:01 -0700

    returning sum of n

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function that returns sum of all data(n)
+ * @head: head pointer
+ *
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
