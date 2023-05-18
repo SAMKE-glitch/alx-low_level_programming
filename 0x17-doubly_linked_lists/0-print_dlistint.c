f9e1bff4bb5add6175014f4084216710bb61f03a
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-18 08:50:37 -0700

    printing elements in a list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints elements of a linked list
+ * @h: head pointer to the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h_next;
+	}
+
+	return (count);
+}
