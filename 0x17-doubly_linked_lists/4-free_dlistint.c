5bcf24d4ac3f0ac1eb0d414fae8ca214fff508b4
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-18 10:17:24 -0700

    freeing a doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_dlistint - function that frees a doubly linked list
+ * @head: pointer to the list
+ *
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_int *temp;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->next;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
