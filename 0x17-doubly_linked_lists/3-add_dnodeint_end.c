2b0e61e9bb441a3b990b36a5b9a980bd3201b73a
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-18 10:00:21 -0700

    adding node at the of a list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a node  at the end of a double linked list
+ * @head: pointer to the doubly linked list
+ * @n: the element of node to be added
+ *
+ * Return: the new doubly linked list
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *h;
+	dlistint_t *new_node;
+
+	new_node = malloc(sozeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new_node;
+	}
+	else
+	{
+		*head = new_node;
+	}
+	new_node->prev = h;
+
+	return (new_node);
+}
