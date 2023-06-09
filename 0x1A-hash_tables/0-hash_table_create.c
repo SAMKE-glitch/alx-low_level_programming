92d353a711b7d3b07ed2530b8893fd293d096d16
SAMKE-glitch <sammythemwa@gmail.com>
2023-06-09 00:36:39 -0700

    created hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - a function pointer that creates a hash table
+ * @size: The size of the array in this case hash table
+ *
+ * Return: If on errors then NULL, or a pointer to the new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table_created;
+	unsigned long int i;
+
+	table_created = malloc(sizeof(hash_table_t));
+
+	if (table_created == NULL)
+		return (NULL);
+
+	table_created->size = size;
+	table_created->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (table_created->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		table_created->array[i] = NULL;
+	}
+
+	return (table_created);
+}
