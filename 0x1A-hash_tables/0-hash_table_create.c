865e3c82fd7870386a8d1d20093fdb218a491107
SAMKE-glitch <sammythemwa@gmail.com>
2023-06-09 00:50:45 -0700

    djb2 algorithim for hashtbales

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table_created->size = size;
 	table_created->array = malloc(sizeof(hash_node_t *) * size);
-
 	if (table_created->array == NULL)
 		return (NULL);
 
