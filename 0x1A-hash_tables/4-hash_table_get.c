f4bc42b79f4558912edef2cacd0a761b4a3383ab
SAMKE-glitch <sammythemwa@gmail.com>
2023-06-09 01:41:01 -0700

    retrieving value with associated key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves the value associated with the key
+ * @ht: A pointer to the hash table
+ * @key: The key to get the value from.
+ *
+ * Return: If key can't be matched - NULL otherwise -  the value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}
