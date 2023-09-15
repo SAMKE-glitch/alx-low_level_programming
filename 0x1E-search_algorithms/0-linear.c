8ccab72023c2beb8c5939d741e6b73e1ce9cd563
SAMKE-glitch <sammythemwa@gmail.com>
2023-09-15 02:51:17 -0700

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -20,7 +20,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (x = 0; x < size; x++)
 	{
-		printf("Value checked array[%lu] = [%i]\n", x, array[x]);
+		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
 		if (array[x] == value)
 		{
 			return (x);
